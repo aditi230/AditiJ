4d2abe8e31f08c1f84bd70fc73e3e542c2499f44
aditi230 <56488242+aditi230@users.noreply.github.com>
2020-05-14 20:42:47 +0530

    Add files via upload

---
+++ Screenshot.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import pyautogui  #install it
+
+win= tk.Tk()
+win.title("Take Screenshot")
+win.geometry('300x300')
+
+def takescreenshot():
+    screenshot = pyautogui.screenshot()
+    screenshot.save('Desktop\\screenshot.png')
+
+button = tk.Button(win,text="Capture Screenshot",command=takescreenshot)
+button.pack()
+
+win.mainloop()
---
+++ WordToPdf.py
@@ -0,0 +1,5 @@
+# word to pdf converte
+
+from docx2pdf import convert
+
+convert(r"C:\User\Dell\Desktop\data.docx", r"C:\User\Dell\Desktop\data.pdf")
